9cce4a80b2082e86ca8b9d1f97972e6d5e24628a
vagrant <vagrant@nurse-patients>
2021-10-15 16:49:14 +0000

    seeding 50k patients with null blood_pressure working

--- database/migrations/2021_10_15_161139_create_patients_table.php
+++ database/migrations/2021_10_15_161139_create_patients_table.php
@@ -14,7 +14,7 @@ class CreatePatientsTable extends Migration
     public function up()
     {
         Schema::create('patients', function (Blueprint $table) {
-            $table->id();
+            $table->bigIncrements('id');
             $table->string('name');
             $table->integer('blood_pressure')->nullable();
             $table->timestamps();
